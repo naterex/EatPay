c3fbc5ff53b73cba3ec4770167a7322bec415ed3
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-08-11 16:11:50 +0800

    Add Role seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Role.where(name: "Admin").first_or_create
+Role.where(name: "Manager").first_or_create
+Role.where(name: "Chef").first_or_create
+Role.where(name: "Waiter").first_or_create
