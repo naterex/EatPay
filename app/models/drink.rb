bb74a25db97e3bdcdd56d271dd84e9c623c0263a
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-08-14 22:02:50 +0800

    Add titleize method for food name and drink name

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -3,4 +3,11 @@ class Drink < ActiveRecord::Base
 	has_many :drinks_order, dependent: :destroy
 	validates :name, :presence => true, :uniqueness => true
 
+	before_save :capitalize
+
+	private
+	   def capitalize
+	     self.name = self.name.titleize 
+	   end
+
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,11 @@
 class Food < ActiveRecord::Base
 	has_many :foods_order, dependent: :destroy
 	validates :name, :presence => true, :uniqueness => true
+
+	before_save :capitalize
+
+	private
+	   def capitalize
+	     self.name = self.name.titleize 
+	   end
 end
