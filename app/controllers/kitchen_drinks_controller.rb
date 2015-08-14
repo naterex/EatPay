51e7e597e03d986b1a3fb632fd146a4511e69412
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-08-14 21:41:53 +0800

    Fix kitchen_foods and kitchen_drinks controller: specify join and where conditions on joined table

--- app/controllers/kitchen_drinks_controller.rb
+++ app/controllers/kitchen_drinks_controller.rb
@@ -3,6 +3,7 @@ class KitchenDrinksController < ApplicationController
 	before_action :authenticate_user!
 	
   def index
-  	@orders = Order.all.order(created_at: :asc)
+  	#@orders = Order.all.order(created_at: :asc)
+  	@orders = Order.joins(:drinks_orders).where('drinks_orders.status' => 0).uniq
   end
 end
--- app/controllers/kitchen_foods_controller.rb
+++ app/controllers/kitchen_foods_controller.rb
@@ -3,6 +3,7 @@ class KitchenFoodsController < ApplicationController
 	before_action :authenticate_user!
 	
   def index
-  	@orders = Order.all.order(created_at: :asc)
+  	#@orders = Order.all.order(created_at: :asc)
+  	@orders = Order.joins(:foods_orders).where('foods_orders.status' => 0).uniq
   end
 end
