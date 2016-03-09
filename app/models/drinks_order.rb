5fea92f61bd83feeaa2656f4fb08da6fbfe42ec0
Nate Welling <nate@natewelling.com>
2016-03-09 11:16:13 +0800

    add prices to food/drinks & total to orders

--- app/models/drinks_order.rb
+++ app/models/drinks_order.rb
@@ -1,4 +1,4 @@
 class DrinksOrder < ActiveRecord::Base
 	belongs_to :drink
-  	belongs_to :order
+	belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ActiveRecord::Base
 	has_many :foods_orders
-  	has_many :foods, through: :foods_orders
+	has_many :foods, through: :foods_orders
 
-  	has_many :drinks_orders
-  	has_many :drinks, through: :drinks_orders
-  
-  	accepts_nested_attributes_for :foods_orders, allow_destroy: true
-  	accepts_nested_attributes_for :drinks_orders, allow_destroy: true
+	has_many :drinks_orders
+	has_many :drinks, through: :drinks_orders
+
+	accepts_nested_attributes_for :foods_orders, allow_destroy: true
+	accepts_nested_attributes_for :drinks_orders, allow_destroy: true
 end
