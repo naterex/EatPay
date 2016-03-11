fb0a188f2ca6b299b99ca183a4771805e2d9f76a
Nate Welling <nate@natewelling.com>
2016-03-11 12:20:28 +0800

    add food & drink items

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,105 @@ User.create!(
   sign_in_count: 0,
   role_id: "1"
 )
+
+# 10 Food items
+Food.create!{
+  name: "Nasi Lemak",
+  price: 10
+}
+
+Food.create!{
+  name: "Nasi Ayam",
+  price: 8
+}
+
+Food.create!{
+  name: "Pork Noodle",
+  price: 7
+}
+
+Food.create!{
+  name: "Chicken Chop",
+  price: 14
+}
+
+Food.create!{
+  name: "Burger Ayam",
+  price: 9
+}
+
+Food.create!{
+  name: "Kebab",
+  price: 5
+}
+
+Food.create!{
+  name: "Bak Kut Teh",
+  price: 25
+}
+
+Food.create!{
+  name: "Chao Kuey Teow",
+  price: 6
+}
+
+Food.create!{
+  name: "Rojak",
+  price: 4
+}
+
+Food.create!{
+  name: "Yong Tau Foo",
+  price: 8
+}
+
+# 10 Drink item
+Drink.create!{
+  name: "Teh Ais",
+  price: 3
+}
+
+Drink.create!{
+  name: "Milo Ais",
+  price: 5
+}
+
+Drink.create!{
+  name: "Sirap Limau",
+  price: 5
+}
+
+Drink.create!{
+  name: "Orange Juice",
+  price: 7
+}
+
+Drink.create!{
+  name: "Tequila ",
+  price: 3
+}
+
+Drink.create!{
+  name: "Asahi Beer",
+  price: 8
+}
+
+Drink.create!{
+  name: "Sky Juice",
+  price: 2
+}
+
+Drink.create!{
+  name: "Kopi O",
+  price: 4
+}
+
+Drink.create!{
+  name: "Red Wine",
+  price: 23
+}
+
+Drink.create!{
+  name: "Cane Juice",
+  price: 4
+}
