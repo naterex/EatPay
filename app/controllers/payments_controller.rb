9587eee56eda5782d1bfb32a24eca650fa3dc16d
Hart <mwpro_25@hotmail.com>
2016-03-16 01:40:02 +0800

    removed before_action authenticate_user

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,5 @@
 class PaymentsController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :find_order, only: [:new, :create]
 
   def new
